3d073cad477a2ff313b48281e3b28d3a80007de6
elizabeth <elizahwambosha@gmail.com>
2024-12-06 16:56:01 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+console.log(customerName.toUpperCase(customerName))
+
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+  }
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"; 
+  }
+  
+
+const leastFavoriteCustomer = "Raila";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "change"; 
+}
